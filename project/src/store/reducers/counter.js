72a35d5e3508564083d7e655e88316492d3d8575
zhuaofei <2353536563@qq.com>
2018-09-24 15:39:28 +0800

    update

--- project/src/store/reducers/counter.js
+++ project/src/store/reducers/counter.js
@@ -1,32 +1,24 @@
-import {
-  handleActions
-} from 'redux-actions'
-import {
-  INCREMENT,
-  DECREMENT,
-  ASYNC_INCREMENT
-} from '../types/counter'
-
-export default handleActions({
-  [INCREMENT](state) {
-    return {
-      ...state,
-      num: state.num + 1
-    }
-  },
-  [DECREMENT](state) {
-    return {
-      ...state,
-      num: state.num - 1
-    }
-  },
-  [ASYNC_INCREMENT](state, action) {
-    return {
-      ...state,
-      asyncNum: state.asyncNum + action.payload
-    }
-  }
-}, {
+import { handleActions } from 'redux-actions'
+import { INCREMENT, DECREMENT, ASYNC_INCREMENT } from '../types/counter'
+const initState = {
+  name: '',
   num: 0,
   asyncNum: 0
-})
+}
+
+export default handleActions({
+  [INCREMENT](state, actions) {
+    console.log(state, actions)
+    return { ...state, num: state.num + 1 }
+  },
+
+  [DECREMENT](state) {
+    return { ...state, num: state.num - 1 }
+  },
+
+  [ASYNC_INCREMENT](state, action) {
+    return { ...state, asyncNum: state.asyncNum + action.payload }
+  }
+
+}, { num: 0, asyncNum: 0 })
+// , { num: 0, asyncNum: 0 }
