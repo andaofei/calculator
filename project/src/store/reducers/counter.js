bdc552b24a9691f68466363a89312aad1be48629
zhuaofei <2353536563@qq.com>
2018-10-29 00:14:06 +0800

    update

--- project/src/store/reducers/counter.js
+++ project/src/store/reducers/counter.js
@@ -1,11 +1,42 @@
 import { handleActions } from 'redux-actions'
-import { INCREMENT, DECREMENT, ASYNC_INCREMENT, SET_IMAGES } from '../types/counter'
+import {
+  INCREMENT,
+  DECREMENT,
+  ASYNC_INCREMENT,
+  SET_IMAGES,
+  PACK_TOTAL,
+  CURT_TOTAL,
+  FLAT_TOTAL,
+  WATER_TOTAL,
+  SUCK_TOTAL
+} from '../types/counter'
 
 export default handleActions({
   [INCREMENT](state, actions) {
-    // console.log(state, actions)
     return { ...state, num: state.num + 1, selectId: actions.payload.id }
-  },
+  }, // 选择tabID
+
+  [PACK_TOTAL](state, actions) {
+    // console.log(actions, 'PACK_TOTAL')
+    return { ...state, packTotal: actions.payload }
+  }, // 靠包总数
+
+  [CURT_TOTAL](state, actions) {
+    // console.log(actions, 'CURT_TOTAL')
+    return { ...state, curtTotal: actions.payload }
+  }, // 窗帘总数
+
+  [FLAT_TOTAL](state, actions) {
+    return { ...state, flatTotal: actions.payload }
+  }, // 平板帘头
+
+  [WATER_TOTAL](state, actions) {
+    return { ...state, waterTotal: actions.payload }
+  }, // 水波帘头
+
+  [SUCK_TOTAL](state, actions) {
+    return { ...state, SuckTotal: actions.payload }
+  }, // 抽折帘头
 
   [DECREMENT](state) {
     return { ...state, num: state.num - 1 }
@@ -19,4 +50,14 @@ export default handleActions({
     // console.log(state, actions.payload.img)
     return { ...state, imagesUrl: actions.payload.img }
   }
-}, { num: 0, asyncNum: 0, selectId: '', imagesUrl: [] })
+}, {
+  num: 0,
+  asyncNum: 0,
+  selectId: '',
+  imagesUrl: [],
+  packTotal: [],
+  curtTotal: [],
+  waterTotal: [],
+  flatTotal: [],
+  SuckTotal: []
+})
--- project/src/store/types/counter.js
+++ project/src/store/types/counter.js
@@ -1,7 +1,17 @@
-export const INCREMENT = 'INCREMENT'
+export const INCREMENT = 'INCREMENT' // 选择首页tab
 
 export const DECREMENT = 'DECREMENT'
 
 export const ASYNC_INCREMENT = 'ASYNC_INCREMENT'
 
 export const SET_IMAGES = 'SET_IMAGES'
+
+export const PACK_TOTAL = 'PACK_TOTAL' // 靠枕
+
+export const CURT_TOTAL = 'CURT_TOTAL' // 窗帘
+
+export const FLAT_TOTAL = 'FLAT_TOTAL' // 平板帘头
+
+export const WATER_TOTAL = 'WATER_TOTAL' // 水波帘头
+
+export const SUCK_TOTAL = 'SUCK_TOTAL' // 帘头
